d708d541fa30e0946768ff446722d585ca5138ce
ISHIKA SOLANKI <ishikasolanki682@gmail.com>
2025-05-12 10:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function firstNonRepeatedChar(str) {
-  const charCount = {};
-  for (let char of str) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
-
-
-  for (let char of str) {
-    if (charCount[char] === 1) {
-      return char;
+ const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
     }
-  }
-
-  return null; 
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
+console.log(firstNonRepeatedChar("aabbcdd"));
+console.log(firstNonRepeatedChar("aabbcc"));
+//const input = prompt("a");
+//for (let index = 0; index < bound; inde++) {
+	
+//}
+//alert(firstNonRepeatedChar(input));
