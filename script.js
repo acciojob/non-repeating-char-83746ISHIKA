898a14c35998d4a0797a1ce9bd59996d53d1cc39
ISHIKA SOLANKI <ishikasolanki682@gmail.com>
2025-05-12 10:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function firstNonRepeatedChar(str) {
-  
-    const charCount = new Map();
-    
-    for (let char of str) {
-        charCount.set(char, (charCount.get(char) || 0) + 1);
-    }
+  const charCount = {};
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
 
-    for (let char of str) {
-        if (charCount.get(char) === 1) {
-            return char;
-        }
+
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
     }
-    
-    return null;
+  }
+
+  return null; 
 }
-
-
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input));
